0029adfb6a56d7f6c75a3f08e417f2e8933bc1e4
max-konin <mk@kernel-corp.com>
2013-11-25 15:34:04 +0700

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vector::Application.config.secret_token = '4347c63348b0800d1ce51eca82c825169049ef0e4b0f6c16cda64fdf1a3443827a946433f2ce30dfe6861184d23c4a8817017a76fe7e3282570970d9143e1268'
