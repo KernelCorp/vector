823d32c98f9fdf1516ba41efff7707a266563e50
Zamyatin-AA <stekbox@gmail.com>
2013-12-05 18:56:02 +0700

    Galleries done.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,14 @@ class PagesController < ApplicationController
 
   def show
     if @page.redirect.blank?
-      render @page.redirect
-    else
       render
+    else
+      if @page.redirect == '/floor_galleries'
+        redirect_to floor_galleries_path(page_id: @page.id)
+      end
+      if @page.redirect == '/ceiling_galleries'
+        redirect_to ceiling_galleries_path(page_id: @page.id)
+      end
     end
   end
 
