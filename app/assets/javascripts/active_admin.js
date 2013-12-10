19b7160c1f44ee0bbbadfc61d653a0e5c7617e02
max-konin <mk@kernel-corp.com>
2013-12-10 19:15:42 +0700

    brutefix 4

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,13 @@
 //= require active_admin/base
+//= require fileuploader
+
+//= require jquery
+//= require jquery_ujs
+
+//= require jquery-ui
+//= require jquery.minicolors
+//= require tinymce
+//= require admin_lib/init_tinymce
+//= require_tree ./admin_lib
+
+
