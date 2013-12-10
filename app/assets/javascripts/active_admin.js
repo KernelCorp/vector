17028e4cecf3946fb81f28d7a04eb126bf31df0d
max-konin <mk@kernel-corp.com>
2013-12-10 18:09:28 +0700

    try to fix js

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,13 +1,12 @@
 //= require active_admin/base
-//= require fileuploader
 
 //= require jquery
 //= require jquery_ujs
 
 //= require jquery-ui
 //= require jquery.minicolors
-//= require tinymce
-//= require admin_lib/init_tinymce
+
 //= require_tree ./admin_lib
+
 
 
