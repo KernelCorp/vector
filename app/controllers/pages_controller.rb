b031c175249aa1ebe67cfcce985711b64afdb12f
mitchenkod <mitchenko1897@mail.ru>
2013-12-10 21:54:12 +0700

    bugs with lamps and routes are fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,28 +5,28 @@ class PagesController < ApplicationController
       render
     else
       if @page.redirect == '/floor_galleries'
-        redirect_to floor_galleries_path(page_id: @page.id)
+        redirect_to page_floor_galleries_path(@page)
       end
       if @page.redirect == '/ceiling_galleries'
-        redirect_to ceiling_galleries_path(page_id: @page.id)
+        redirect_to page_ceiling_galleries_path(@page)
       end
       if @page.redirect == '/floor_spectrums'
-        redirect_to floor_spectrums_path(page_id: @page.id)
+        redirect_to page_floor_spectrums_path(@page)
       end
       if @page.redirect == '/ceiling_spectrums'
-        redirect_to ceiling_spectrums_path(page_id: @page.id)
+        redirect_to page_ceiling_spectrums_path(@page)
       end
       if @page.redirect == '/outdoor_lamps'
-        redirect_to lamps_path(page_id: @page.id, category_id: 1)
+        redirect_to page_category_lamps_path(@page, category_id: 1)
       end
       if @page.redirect == '/indoor_lamps'
-        redirect_to lamps_path(page_id: @page.id, category_id: 2)
+        redirect_to page_category_lamps_path(@page, category_id: 2)
       end
       if @page.redirect == '/light_panels'
-        redirect_to lamps_path(page_id: @page.id, category_id: 3)
+        redirect_to page_category_lamps_path(@page, category_id: 3)
       end
       if @page.redirect == '/light_tapes'
-        redirect_to lamps_path(page_id: @page.id, category_id: 4)
+        redirect_to page_category_lamps_path(@page, category_id: 4)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,20 @@ Vector::Application.routes.draw do
 
   root to: 'main_page#index'
 
-  resources :pages, only: [:show], path: 'pages'
+  resources :pages, only: [:show], path: 'pages' do
+    resources :ceiling_galleries, only: [:index]
+    resources :floor_galleries, only: [:index]
+    resources :floor_spectrums, only: [:index]
+    resources :ceiling_spectrums, only: [:index]
+    resources :categories, only: [:show], path: '' do
+      resources :lamps, only: [:index]
+    end
+  end
 
-  resources :ceiling_galleries, only: [:index]
-  resources :floor_galleries, only: [:index]
 
 
-  resources :floor_spectrums, only: [:index]
-  resources :ceiling_spectrums, only: [:index]
 
-  resources :lamps, only: [:index]
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
