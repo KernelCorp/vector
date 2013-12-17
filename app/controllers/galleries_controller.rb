b28a5b8569ddd318ffa51512e94bd611f0bae59c
Zamyatin-AA <stekbox@gmail.com>
2013-12-17 17:27:54 +0700

    Mailer phone fixed.

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,10 +1,7 @@
 class GalleriesController < ApplicationController
   def index
-    #TODO: remove this string after layout will be finished
     clazz = params[:class_name] ==  'FloorGallery' ? FloorGallery : CeilingGallery
     @galleries = clazz.all
   end
-
-
 
 end
